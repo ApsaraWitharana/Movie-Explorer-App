1827ef7789ba24d768dc04311472d19f43d08658
ApsaraWitharana <asachini095@gmail.com>
2025-05-09 10:34:32 +0530

    add-types of movie

---
+++ src/types/movie.ts
@@ -0,0 +1,57 @@
+export interface Movie {
+  id: number;
+  title: string;
+  poster_path: string | null;
+  backdrop_path: string | null;
+  overview: string;
+  release_date: string;
+  vote_average: number;
+  vote_count: number;
+  genre_ids: number[];
+  popularity: number;
+  adult: boolean;
+  original_language: string;
+  original_title: string;
+  video: boolean;
+}
+
+export interface MovieDetails extends Movie {
+  belongs_to_collection: null | {
+    id: number;
+    name: string;
+    poster_path: string | null;
+    backdrop_path: string | null;
+  };
+  budget: number;
+  genres: Array<{
+    id: number;
+    name: string;
+  }>;
+  homepage: string | null;
+  imdb_id: string | null;
+  production_companies: Array<{
+    id: number;
+    logo_path: string | null;
+    name: string;
+    origin_country: string;
+  }>;
+  production_countries: Array<{
+    iso_3166_1: string;
+    name: string;
+  }>;
+  revenue: number;
+  runtime: number;
+  spoken_languages: Array<{
+    iso_639_1: string;
+    name: string;
+  }>;
+  status: string;
+  tagline: string | null;
+}
+
+export interface TMDBResponse<T> {
+  page: number;
+  results: T[];
+  total_pages: number;
+  total_results: number;
+}
